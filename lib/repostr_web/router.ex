6520526e3f4b443c602461f714ec71361c20c7e0
Matej <matej@occupationbliss.com>
2025-03-11 10:43:03 +0100

    Initial commit.

--- lib/repostr_web/router.ex
+++ lib/repostr_web/router.ex
@@ -1,6 +1,8 @@
 defmodule RepostrWeb.Router do
   use RepostrWeb, :router
 
+  import RepostrWeb.AdminAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule RepostrWeb.Router do
     plug :put_root_layout, html: {RepostrWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_admin
   end
 
   pipeline :api do
@@ -51,6 +54,44 @@ defmodule RepostrWeb.Router do
 
       live_dashboard "/dashboard", metrics: RepostrWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", RepostrWeb do
+    pipe_through [:browser, :redirect_if_admin_is_authenticated]
+
+    live_session :redirect_if_admin_is_authenticated,
+      on_mount: [{RepostrWeb.AdminAuth, :redirect_if_admin_is_authenticated}] do
+      live "/admins/register", AdminRegistrationLive, :new
+      live "/admins/log_in", AdminLoginLive, :new
+      live "/admins/reset_password", AdminForgotPasswordLive, :new
+      live "/admins/reset_password/:token", AdminResetPasswordLive, :edit
+    end
+
+    post "/admins/log_in", AdminSessionController, :create
+  end
+
+  scope "/", RepostrWeb do
+    pipe_through [:browser, :require_authenticated_admin]
+
+    live_session :require_authenticated_admin,
+      on_mount: [{RepostrWeb.AdminAuth, :ensure_authenticated}] do
+      live "/admins/settings", AdminSettingsLive, :edit
+      live "/admins/settings/confirm_email/:token", AdminSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", RepostrWeb do
+    pipe_through [:browser]
+
+    delete "/admins/log_out", AdminSessionController, :delete
+
+    live_session :current_admin,
+      on_mount: [{RepostrWeb.AdminAuth, :mount_current_admin}] do
+      live "/admins/confirm/:token", AdminConfirmationLive, :edit
+      live "/admins/confirm", AdminConfirmationInstructionsLive, :new
     end
   end
 end
